b6a1f48f2ff000ea075794c99b324edd1a990d8b
JoelBaumann1 <100359477+JoelBaumann1@users.noreply.github.com>
2024-03-13 15:50:43 +0100

    kinda working

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,6 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 
+import SpectrogramPlayer from "react-audio-spectrogram-player";
+import sxx from "./res/19-198-0001.json";
+import src from "./res/file_example.wav";
+
+
+
 function App() {
   return (
     <div className="App">
@@ -17,6 +23,17 @@ function App() {
         >
           Learn React
         </a>
+        <SpectrogramPlayer
+            src={src}
+            sxx={sxx}
+            specHeight={200}
+            navHeight={50}
+            navigator
+            settings
+            colormap="viridis"
+            transparent
+            dark
+        />
       </header>
     </div>
   );
